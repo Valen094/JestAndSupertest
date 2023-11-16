a3d1d36e2c475b8a6b3d4ae8c72ae343d13e2881
valen094 <ramirezvalentina783@gmail.com>
2023-11-15 21:58:36 -0500

    Inicio y fin de peticiones POST

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-import Express from "express";
+import express from "express";
+import {v4} from 'uuid'
 
-const app = Express()
+const app = express();
+
+app.use(express.json());
+//cualquier peticion que llegue la convierte en un formato JSON
 
 app.get('/ping', (req, res) =>{
     res.send('pong')
@@ -10,6 +14,19 @@ app.get('/tasks', (req, res) =>{
     res.json([]
         // {tasks: []}
         )
+});
+
+app.post('/tasks', (req, res) =>{
+    const {title, description} = req.body;
+
+    //valida si existe el titulo y descripción
+    if (!title || !description) return res.sendStatus(400);
+
+    res.json({
+        title,
+        description,
+        id: v4(),
+    });
 });
 
 export default app;
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -18,3 +18,47 @@ describe('GET /tasks', ()=>{
     });
 
 });
+
+describe('POST /tasks', () =>{
+    describe('given a title and description', () =>{
+        const newTask = {
+            title: "Test Task",
+            description: "Test Description"
+        }
+
+        test('should respond with a 200 status code', async () =>{
+            const response = await request(app).post('/tasks').send(newTask);
+            expect(response.statusCode).toBe(200);
+        });
+    
+        test('should have a content-type: application/json in header', async () =>{
+            const response = await request(app).post('/tasks').send(newTask);
+            expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+            //Que la respuesta que este dando sea igual a un JSON
+        });
+    
+        test('should respond with an task ID', async () =>{
+            const response = await request(app).post('/tasks').send(newTask);
+            expect(response.body.id).toBeDefined();
+            //body contiene el cuerpo de la petición
+        })
+    });
+
+        //Comprueba si es titulo y la descripción existe
+        describe('when title and description in missing', () =>{
+        test('should erespond with a 400 status code', async () =>{
+            const fields = [
+                {},
+                {title: 'Test Task'},
+                {description: 'Test Description'},
+            ]
+
+            for(const body of fields) {
+                const response = await request(app).post('/tasks').send(body)
+                expect(response.statusCode).toBe(400);
+            }
+
+        });
+
+    })
+});
